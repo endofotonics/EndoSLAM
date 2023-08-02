bb26b8e94088f9b50fb5f1a79339ccb31e96925f
ngchanway <chanwayng@u.nus.edu>
2023-08-02 10:47:56 +0800

    feat: Add inference for mps

--- EndoSfMLearner/run_inference.py
+++ EndoSfMLearner/run_inference.py
@@ -1,11 +1,12 @@
 import torch
 
-from imageio import imread, imsave
-from scipy.misc import imresize
+# from imageio import imread, imsave
+from skimage import transform, io, color
+# from scipy.misc import imresize
 import numpy as np
-from path import Path
+from pathlib import Path
 import argparse
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 from models import DispResNet
 from utils import tensor2array
@@ -27,7 +28,8 @@ parser.add_argument("--img-exts", default=['png', 'jpg', 'bmp'], nargs='*', type
 parser.add_argument('--resnet-layers', required=True, type=int, default=18, choices=[18, 50],
                     help='depth network architecture.')
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+print(f'~~~Using {device}~~~')
 
 
 @torch.no_grad()
@@ -38,29 +40,29 @@ def main():
         return
 
     disp_net = DispResNet(args.resnet_layers, False).to(device)
-    weights = torch.load(args.pretrained)
+    weights = torch.load(args.pretrained, map_location=torch.device(device))
     disp_net.load_state_dict(weights['state_dict'])
     disp_net.eval()
 
     dataset_dir = Path(args.dataset_dir)
     output_dir = Path(args.output_dir)
-    output_dir.makedirs_p()
+    output_dir.mkdir(exist_ok=True)
 
     if args.dataset_list is not None:
         with open(args.dataset_list, 'r') as f:
             test_files = [dataset_dir/file for file in f.read().splitlines()]
     else:
-        test_files = sum([dataset_dir.files('*.{}'.format(ext)) for ext in args.img_exts], [])
+        test_files = sum([list(dataset_dir.glob(f'*.{ext}')) for ext in args.img_exts], [])
 
-    print('{} files to test'.format(len(test_files)))
+    print(f'{len(test_files)} files to test')
 
     for file in tqdm(test_files):
 
-        img = imread(file).astype(np.float32)
+        img = io.imread(file).astype(np.float32)
 
         h, w, _ = img.shape
         if (not args.no_resize) and (h != args.img_height or w != args.img_width):
-            img = imresize(img, (args.img_height, args.img_width)).astype(np.float32)
+            img = transform.resize(img, (args.img_height, args.img_width)).astype(np.float32)
         img = np.transpose(img, (2, 0, 1))
 
         tensor_img = torch.from_numpy(img).unsqueeze(0)
@@ -68,16 +70,22 @@ def main():
 
         output = disp_net(tensor_img)[0]
 
-        file_path, file_ext = file.relpath(args.dataset_dir).splitext()
-        file_name = '-'.join(file_path.splitall())
+        # file_path, file_ext = file.relpath(args.dataset_dir).splitext()
+        file_name = Path('-'.join(file.relative_to(args.dataset_dir).parts))
 
         if args.output_disp:
             disp = (255*tensor2array(output, max_value=None, colormap='bone')).astype(np.uint8)
-            imsave(output_dir/'{}_disp{}'.format(file_name, file_ext), np.transpose(disp, (1, 2, 0)))
+            io.imsave(
+                output_dir/f'{file_name.stem}_disp{file_name.suffix}',
+                (255*color.rgba2rgb(np.transpose(disp, (1, 2, 0)))).astype(np.uint8)
+            )
         if args.output_depth:
             depth = 1/output
             depth = (255*tensor2array(depth, max_value=10, colormap='rainbow')).astype(np.uint8)
-            imsave(output_dir/'{}_depth{}'.format(file_name, file_ext), np.transpose(depth, (1, 2, 0)))
+            io.imsave(
+                output_dir/f'{file_name.stem}_depth{file_name.suffix}',
+                (255*color.rgba2rgb(np.transpose(depth, (1, 2, 0)))).astype(np.uint8)
+            )
 
 
 if __name__ == '__main__':
